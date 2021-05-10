0bc6a07a8cb3b0cf8deec41a1bebf6a3b3acd601
ptka0426 <longtao0426@163.com>
2021-05-10 11:49:54 +0800

    maven

--- src/main/java/com/example/boranqichacha/BoranQichachaApplication.java
+++ src/main/java/com/example/boranqichacha/BoranQichachaApplication.java
@@ -1,14 +1,20 @@
 package com.example.boranqichacha;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BoranQichachaApplication {
 
+    private static final Logger logger = LoggerFactory.getLogger(BoranQichachaApplication.class);
+
     public static void main(String[] args) {
+        logger.info("\n===============项目启动了===============");
         SpringApplication.run(BoranQichachaApplication.class, args);
+        logger.info("\n===============启动成功了===============");
     }
 
 }
